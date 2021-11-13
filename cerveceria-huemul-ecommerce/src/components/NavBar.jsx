62c82cde002833ca2d087041779e3047747d74a2
Esteban Diaz <82244428+EstebanD90@users.noreply.github.com>
2021-11-13 19:36:57 -0300

    Add files via upload

--- cerveceria-huemul-ecommerce/src/components/NavBar.jsx
+++ cerveceria-huemul-ecommerce/src/components/NavBar.jsx
@@ -1,40 +1,40 @@
-// import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
-
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import CartWidget from './CartWidget'
+import React from "react";
 
 const NavBar = () => {
-    return (       
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Fede El Mejor Ecommerce</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                        <Nav.Link href="#features">Gorras</Nav.Link>
-                        <Nav.Link href="#pricing">Remeras</Nav.Link>
-                        <Nav.Link href="#pricing">zapas</Nav.Link>
-                        {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown> */}
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">
-                                <CartWidget />
-                            </Nav.Link>
-                            
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>       
-    )
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                </div>
+            </div>
+            </nav>
+        </div>
+
+        
+    );
 }
 
-export default NavBar
+
+export default NavBar;
+
+
