59bebe0aefd7a3ce14ceb64060aadaa3a8386d1d
Esteban Diaz <82244428+EstebanD90@users.noreply.github.com>
2021-11-13 18:56:35 -0300

    Add files via upload

---
+++ cerveceria-huemul-ecommerce/src/components/MenuItems.jsx
@@ -0,0 +1,33 @@
+const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Productos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Servicios',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Quienes Somos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contacto',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Carrito',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+]
