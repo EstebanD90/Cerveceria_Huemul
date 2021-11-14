884173b780deb5e0d07591423c213819f8499434
Esteban Diaz <82244428+EstebanD90@users.noreply.github.com>
2021-11-13 21:06:27 -0300

    Add files via upload

--- cerveceria-huemul-ecommerce/src/App.js
+++ cerveceria-huemul-ecommerce/src/App.js
@@ -1,15 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-      <div className="App">
-          <NavBar />
-
-      </div>
-
+    <div className="App">
+        <NavBar/>
+          <ItemListContainer/>
+    </div>
   );
 }
 
 export default App;
-
