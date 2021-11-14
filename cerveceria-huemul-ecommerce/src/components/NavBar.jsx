de606abe172ada31e43e9cc7b69107f7ea0c4626
Esteban Diaz <82244428+EstebanD90@users.noreply.github.com>
2021-11-13 21:06:44 -0300

    Add files via upload

--- cerveceria-huemul-ecommerce/src/components/NavBar.jsx
+++ cerveceria-huemul-ecommerce/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import CartWidget from './CartWidget'
 
 
 const NavBar = () => {
@@ -10,25 +11,26 @@ const NavBar = () => {
 
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Cervezas Huemul</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#products">Productos</Nav.Link>
+                        <Nav.Link href="#services">Servicios</Nav.Link>
+                        <Nav.Link href="#about-us">Quienes Somos</Nav.Link>
+                        <Nav.Link href="#contact">Contacto</Nav.Link>
+                        {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown>*/}
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                            </Nav.Link>
+                            <Nav.Link href="#cart"><CartWidget/>
+                                </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -40,5 +42,3 @@ const NavBar = () => {
 
 
 export default NavBar;
-
-
