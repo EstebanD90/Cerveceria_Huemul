8ee5c9e0ca1bb8cb73ad42968284cbc01bb5440d
Esteban Diaz <82244428+EstebanD90@users.noreply.github.com>
2021-11-26 20:55:09 -0300

    Add files via upload

--- cerveceria-huemul-ecommerce/src/components/Item/Item.jsx
+++ cerveceria-huemul-ecommerce/src/components/Item/Item.jsx
@@ -1,13 +1,25 @@
 import "./Item.css"
+import ItemCount from "../ItemCount/ItemCount";
 
-const Item = (props) => {
+const Item = ({name, price, description, imgUrl}) => {
+
+    function onAdd(cant) {
+        console.log(cant)
+    }
+
+
     return (
-        <div className={props.name}>
-            <h1>{props.name}</h1>
-            <h2>Precio: {props.price}</h2>
-            <h3>{props.description}</h3>
-            <img src={props.image} alt="" />
+        <><div className={name}>
+            <h1>{name}</h1>
+            <h2>Precio: ${price}</h2>
+            <h3>{description}</h3>
+            <img src={imgUrl} style={{ width: 300, height: 350, marginLeft: 10, marginRight: 10 ,paddingBottom: 10}} alt="" />
+
+                <div className="count">
+                <ItemCount initial={1} stock={10} onAdd={onAdd} style={{ width: 300, height: 300, marginLeft: 10, marginRight: 10 }}/>
+                </div>
         </div>
+        </>
 
     )
 }
