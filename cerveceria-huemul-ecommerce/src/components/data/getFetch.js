9db7b6e6e9c8ec8eabeb2b6aa37809ae686033f5
Esteban Diaz <82244428+EstebanD90@users.noreply.github.com>
2021-11-26 21:19:48 -0300

    Add files via upload

---
+++ cerveceria-huemul-ecommerce/src/components/data/getFetch.js
@@ -0,0 +1,53 @@
+import img_golden from '../../images/cerveza-rubia.jpg'
+import img_irish from '../../images/cerveza-irish-red.jpg'
+import img_ipa from '../../images/cerveza-IPA2.jpg'
+import img_porter from '../../images/cerveza-porter.jpg'
+import img_apa from '../../images/cerveza-APA.jpg'
+import img_stout from '../../images/cerveza-stout.jpg'
+
+
+export const items = [
+    {
+        id: 1,
+        name: "Golden", 
+        price: 220,
+        description: "Cerveza Golden",
+        imgUrl: img_golden
+    },
+    {
+        id: 2,
+        name: "Irish Red",
+        price: 250,
+        description: "Cerveza Irish Red",
+        imgUrl: img_irish
+    },
+    {
+        id: 3,
+        name: "IPA",
+        price: 270,
+        description: "Indian Pale Ale",
+        imgUrl: img_ipa
+    },
+    {
+        id: 4,
+        name: "Porter",
+        price: 260,
+        description: "Cerveza Porter",
+        imgUrl: img_porter
+    },    
+    {
+        id: 5,
+        name: "APA",
+        price: 260,
+        description: "American Pale Ale",
+        imgUrl: img_apa
+    },
+    {
+        id: 6,
+        name: "Stout",
+        price: 280,
+        description: "Cerveza Stout",
+        imgUrl: img_stout
+    }
+];
+
